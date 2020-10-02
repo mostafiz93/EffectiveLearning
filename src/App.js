36c70df6befdcc59abf6e2a22f16be71a3523d7e
Mostafiz Rahman <mostafiz93@gmail.com>
2020-10-02 13:45:05 +0600

    sample layout added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
 
-import { Button } from 'antd';
+import Sidebar from './components/layout';
 
 function App() {
 	return (
 		<div className='App'>
-			<Button type='primary'>Button</Button>
+			<Sidebar />
 		</div>
 	);
 }
