0164cc4ad55900573da3e99b6e92012333ec4448
Simanta Deb Turja <simantaturja@gmail.com>
2020-10-08 01:38:17 +0600

    add topic component

---
+++ src/components/topic/TOPICS.js
@@ -0,0 +1,130 @@
+export const TOPICS = [
+  {
+    id: 0,
+    title: 'Topic - 1',
+    description: 'Bank Management - Liquidity',
+    result: 'Passed',
+    score: '98%',
+    completedon: '15th July, 2020',
+    totalquestions: 5,
+    totalattempts: 9,
+    correctattempts: 5,
+    incorrectattempts: 2
+  },
+
+  {
+    id: 1,
+    title: 'Topic - 2',
+    description: 'Credit Management',
+    result: 'Passed',
+    score: '92%',
+    completedon: '15th July, 2020',
+    totalquestions: 5,
+    totalattempts: 9,
+    correctattempts: 5,
+    incorrectattempts: 2
+  },
+
+  {
+    id: 2,
+    title: 'Topic - 3',
+    description: 'Risk Management Techniques',
+    result: 'Passed',
+    score: '80%',
+    completedon: '15th July, 2020',
+    totalquestions: 5,
+    totalattempts: 9,
+    correctattempts: 5,
+    incorrectattempts: 2
+  },
+
+  {
+    id: 3,
+    title: 'Topic - 4',
+    description: 'Relationship Banking',
+    result: 'Passed',
+    score: '75%',
+    completedon: '15th July, 2020',
+    totalquestions: 5,
+    totalattempts: 9,
+    correctattempts: 5,
+    incorrectattempts: 2
+  },
+
+  {
+    id: 4,
+    title: 'Topic - 5',
+    description: 'Commercial Banking Functions',
+    result: 'Passed',
+    score: '100%',
+    completedon: '15th July, 2020',
+    totalquestions: 5,
+    totalattempts: 9,
+    correctattempts: 5,
+    incorrectattempts: 2
+  },
+  {
+    id: 5,
+    title: 'Topic - 6',
+    description: 'Bank Management - Liquidity',
+    result: 'Passed',
+    score: '100%',
+    completedon: '15th July, 2020',
+    totalquestions: 5,
+    totalattempts: 9,
+    correctattempts: 5,
+    incorrectattempts: 2
+  },
+
+  {
+    id: 6,
+    title: 'Topic - 7',
+    description: 'Credit Management',
+    result: 'Passed',
+    score: '100%',
+    completedon: '15th July, 2020',
+    totalquestions: 5,
+    totalattempts: 9,
+    correctattempts: 5,
+    incorrectattempts: 2
+  },
+
+  {
+    id: 7,
+    title: 'Topic - 8',
+    description: 'Risk Management Techniques',
+    result: 'Passed',
+    score: '100%',
+    completedon: '15th July, 2020',
+    totalquestions: 5,
+    totalattempts: 9,
+    correctattempts: 5,
+    incorrectattempts: 2
+  },
+
+  {
+    id: 8,
+    title: 'Topic - 9',
+    description: 'Relationship Banking',
+    result: 'Passed',
+    score: '100%',
+    completedon: '15th July, 2020',
+    totalquestions: 5,
+    totalattempts: 9,
+    correctattempts: 5,
+    incorrectattempts: 2
+  },
+
+  {
+    id: 9,
+    title: 'Topic - 10',
+    description: 'Commercial Banking Functions',
+    result: 'Passed',
+    score: '100%',
+    completedon: '15th July, 2020',
+    totalquestions: 5,
+    totalattempts: 9,
+    correctattempts: 5,
+    incorrectattempts: 2
+  }
+];
