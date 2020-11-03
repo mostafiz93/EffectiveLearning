8272ac3986429f4f9a7c3e6bc30993debf880641
Asfi Fardous <asfi.ruet2014@gmail.com>
2020-11-03 19:57:13 +0000

    'App' div added to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
-import React from "react";
-import "./App.scss";
-import "./layout.css";
-import Auth from "./components/login/auth";
+import React from 'react';
+import './App.scss';
+import './layout.css';
+import Auth from './components/login/auth';
 import Sidebar from './components/layout';
-import Example from './components/sidebar/example'
-import {
-	BrowserRouter as Router,
-	Switch,
-	Route,
-} from "react-router-dom";
+import Example from './components/sidebar/example';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 class App extends React.Component {
   constructor(props) {
@@ -16,14 +12,16 @@ class App extends React.Component {
   }
 
   render() {
-    return (	
-      <Router>		
-        <Switch>
-            <Route path="/example" component={Example} />
-            <Route path="/home" component={Sidebar} />
-            <Route path="/" component={Auth} />
+    return (
+      <Router>
+        <div className='App'>
+          <Switch>
+            <Route path='/example' component={Example} />
+            <Route path='/home' component={Sidebar} />
+            <Route path='/' component={Auth} />
           </Switch>
-      </Router> 
+        </div>
+      </Router>
     );
   }
 }
